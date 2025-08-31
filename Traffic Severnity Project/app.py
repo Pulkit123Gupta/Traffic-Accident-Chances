dcc62c83d268de76d9ddc56331541073a3740f24
Pulkit123Gupta <122166860+Pulkit123Gupta@users.noreply.github.com>
2025-08-31 11:00:42 +0530

    Update app.py

--- Traffic Severnity Project/app.py
+++ Traffic Severnity Project/app.py
@@ -4,7 +4,7 @@ import pandas as pd
 import joblib
 
 # ------------------- LOAD TRAINED MODEL -------------------
-pipe = joblib.load("accident_model.pkl")
+pipe = joblib.load("models/accident_model.pkl")
 
 # ------------------- PAGE CONFIG -------------------
 st.set_page_config(page_title="Accident Severity Prediction", page_icon="🚦", layout="wide")
@@ -67,3 +67,4 @@ if st.button("🔍 Predict Severity"):
         st.warning(f"⚠️ Predicted Severity: **{severity_label}** ({confidence:.2f}% chance of accident !)")
     else:
         st.error(f"🚨 Predicted Severity: **{severity_label}** ({confidence:.2f}% chance of accident !)")
+
